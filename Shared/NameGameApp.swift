6dcc0fe44a549d7829ad34b1af037358b623ea62
Rob Silverman <robert.silverman@willowtreeapps.com>
2021-02-10 18:25:38 -0500

    Initial Commit

---
+++ Shared/NameGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameGameApp.swift
+//  Shared
+//
+//  Created by Robert Silverman on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct NameGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
